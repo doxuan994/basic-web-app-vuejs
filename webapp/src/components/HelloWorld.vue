dc10de3a57197a2bd5f024a2bb0aeb8b4236fb23
Xuan Do <doxuancntp@gmail.com>
2019-06-06 11:02:03 -0400

    Add, remove, update items.

--- webapp/src/components/HelloWorld.vue
+++ webapp/src/components/HelloWorld.vue
@@ -10,12 +10,47 @@
         </div>
 
 
-
         <div v-html="content"></div>
 
-        <ul>
-            <li v-for="item in items">{{ item.text }}</li>
-        </ul>
+
+        <div class="content">
+
+            <ul>
+                <h3>List 1</h3>
+                <li v-for="(item, index) in items">
+
+                    <button @click="adddOneToSpecie(index)">+</button>
+
+                    {{ item.quantity }}
+
+                    <button @click="removeOneFromSpecie(index)">-</button>
+
+
+
+                    {{ item.text }}
+
+                    <button @click="deleteItem(index)">Make Extinct</button>
+                </li>
+            </ul>
+
+            <ul>
+                <h3>List 2</h3>
+                <li v-for="dino in dinos">
+                    <h4>{{ dino.text | capitalize }}</h4>
+                    <span>The {{ dino.text | undercase }} weights {{ dino.weight }}.</span>
+                    <br>
+                    <a href="">{{ dino.text | undercase | url }}</a>
+                </li>
+            </ul>
+
+            <ul>
+                <li>Total Dinosaurs: {{ totalDinosComputed }} <span>Updated: {{ dinosUpdated }}</span> </li>
+                <li>Total Species: {{ totalSpeciesComputed }} <span>Updated: {{ speciesUpdated }}</span> </li>
+            </ul>
+
+        </div>
+
+
 
 
     </div>
@@ -32,16 +67,77 @@ export default {
                 text: ''
             },
             items: [
-                { text: "Velociraptor" },
-                { text: "Tricerators" },
-                { text: "Stegosaurus" }
+                { text: "Velociraptor", quantity: 5 },
+                { text: "Tricerators", quantity: 3 },
+                { text: "Stegosaurus", quantity: 6 }
             ],
-
+            dinos: [
+                {
+                    text: 'Velociraptor',
+                    weight: '15 kg',
+                },
+                {
+                    text: 'tricerators',
+                    weight: '6,000 kg'
+                },
+                {
+                    text: 'Stegosaurus',
+                    weight: "2,500 kg"
+                }
+            ],
+            totalDinos: 0,
+            totalSpecies: 0,
+            dinosUpdated: 0,
+            speciesUpdated: 0
         }
     },
     methods: {
         addItem() {
             this.items.push(this.item);
+        },
+        deleteItem(index) {
+            this.items.splice(index, 1);
+        },
+        adddOneToSpecie(index) {
+            this.items[index].quantity++;
+        },
+        removeOneFromSpecie(index) {
+            this.items[index].quantity--;
+        }
+    },
+    filters: {
+        capitalize(value) {
+            if (!value) return;
+            value = value.toString();
+            return value.charAt(0).toUpperCase() + value.slice(1);
+        },
+        undercase(value) {
+            if (!value) return;
+            value = value.toString();
+            return value.charAt(0).toLowerCase() + value.slice(1);
+        },
+        url(value) {
+            if (!value) return;
+            value = value.toString();
+
+            return "https://www.wikipedia.org/wiki/" + value;
+        }
+    },
+    computed: {
+        totalDinosComputed() {
+            this.dinosUpdated += 1;
+            let sum = 0;
+            let items = this.items;
+
+            for (let i = 0; i < items.length; i++) {
+                sum += items[i].quantity;
+            }
+
+            return sum;
+        },
+        totalSpeciesComputed() {
+            this.speciesUpdated += 1;
+            return this.items.length;
         }
     }
 }
@@ -65,10 +161,17 @@ button, input {
     font-size: 1em;
 }
 #card {
-    max-width: 460px;
-    border: 3px solid #777;
+    border: 3px solid gold;
     border-radius: 25px;
+
+
 }
+.content {
+    display: flex;
+    justify-content: center;
+}
+
+
 div {
     margin: 1em;
 }
@@ -82,20 +185,23 @@ header {
     font-size: 1.5em;
 }
 ul {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
+
 
     padding: 0;
     margin: 1em;
+
+    margin-top: 2em;
+
+    border: 3px solid gold;
+
+    flex-basis: 460px;
 }
 li {
     list-style: none;
 
     border: 2px solid #ccc;
 
-    margin-top: 0.5em;
-    margin-bottom: 0.5em;
+    margin: 0.5em;
 
     padding: 0.5em 0;
 
